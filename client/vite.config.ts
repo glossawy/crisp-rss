2e17cd3e8278534b4a450d5e1db21cd374d819bf
Glossawy <glossawy@sphorb.email>
2024-04-21 18:51:54 -0400

    prettier

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
